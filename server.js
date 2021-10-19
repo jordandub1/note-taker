d068ab3f64a35de7d215d63199ea3c0775f63761
Jordan DuBose <jordan.dubose1@gmail.com>
2021-10-19 17:05:10 -0400

    fixing refresh issue

--- server.js
+++ server.js
@@ -1,30 +1,28 @@
 const express = require("express");
 const path = require("path");
-const notesData = require("./db/db.json");
 const { Server } = require("http");
 const fs = require("fs");
 // Unique ID NPM Package
-const { v4: uuidv4 } = require('uuid');
-
-const PORT = process.env.PORT || 3001;
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 //HTML route for notes.html
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
 
 //API route for db.json
-app.get("/api/notes", (req, res) => {
-  res.json(notesData);
-});
+app.get("/api/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./db/db.json"))
+);
 
 // GET for notes ID
 app.get("/api/notes/:id", (req, res) =>
@@ -37,32 +35,36 @@ app.get("/*", (req, res) => {
 });
 
 // POST request to add a note
-app.post('/api/notes', (req, res) => {
-    const newSavedNote = req.body;
-    console.log(newSavedNote);
-    newSavedNote.id = uuidv4();
-    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')));
-    console.log(notes);
-    notes.push(newSavedNote);
-    console.log(notes);
-    fs.writeFile('./db/db.json', JSON.stringify(notes), function(err, result) {
-        if(err) console.log('error', err)
-    });
-    return res.json({});
+app.post("/api/notes", (req, res) => {
+  const newSavedNote = req.body;
+  console.log(newSavedNote);
+  newSavedNote.id = uuidv4();
+  const notes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "./db/db.json"))
+  );
+  console.log(notes);
+  notes.push(newSavedNote);
+  console.log(notes);
+  fs.writeFile("./db/db.json", JSON.stringify(notes), function (err, result) {
+    if (err) console.log("error", err);
+  });
+  return res.json({});
 });
 
 //DELETE note
-app.delete('/api/notes/:id', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')));
-    console.log(`delete ${req.params.id}`);
-    let idNote = `${req.params.id}`
-    let deleteNote = notes.findIndex(notes => notes.id === idNote);
-    console.log(deleteNote)
-    notes.splice(deleteNote, 1)
-    fs.writeFile('./db/db.json', JSON.stringify(notes), function(err, result) {
-        if(err) console.log('error', err)
-    });
-    return res.json({});
+app.delete("/api/notes/:id", (req, res) => {
+  const notes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "./db/db.json"))
+  );
+  console.log(`delete ${req.params.id}`);
+  let idNote = `${req.params.id}`;
+  let deleteNote = notes.findIndex((notes) => notes.id === idNote);
+  console.log(deleteNote);
+  notes.splice(deleteNote, 1);
+  fs.writeFile("./db/db.json", JSON.stringify(notes), function (err, result) {
+    if (err) console.log("error", err);
+  });
+  return res.json({});
 });
 
 app.listen(PORT, () => {
